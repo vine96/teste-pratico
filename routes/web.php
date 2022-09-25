4b055e336845c4a5279ebce9d5ae8037c65f9f8f
Vinícius Furtado Pecci de Lima <viniciuspecci@hotmail.com>
2022-09-25 19:05:50 -0300

    Protegendo rotas

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,26 @@ Route::get('/', [App\Http\Controllers\PagesController::class, 'index'])->name('i
 
 Auth::routes();
 
-// Banner routes
-Route::get('/banner', [App\Http\Controllers\PagesController::class, 'indexBanner'])->name('banner');
-Route::post('/salvar-nav', [App\Http\Controllers\PagesController::class, 'saveNavbar'])->name('saveNavbar');
-Route::post('/salvar-banner', [App\Http\Controllers\PagesController::class, 'saveBanner'])->name('saveBanner');
-Route::post('/salvar-centerbar', [App\Http\Controllers\PagesController::class, 'saveCenterbar'])->name('saveCenterbar');
+Route::middleware('auth')->group(function () {
 
-// Firstcard routes
-Route::get('/primeiro-card', [App\Http\Controllers\PagesController::class, 'indexFirstcard'])->name('indexFirstcard');
-Route::post('/primeiro-card/salvar', [App\Http\Controllers\PagesController::class, 'saveFirstcard'])->name('saveFirstcard');
-Route::post('/primeiro-card/salvar-imagens', [App\Http\Controllers\PagesController::class, 'saveFirstcardImages'])->name('saveFirstcardImages');
-Route::get('/primeiro-card/excluir/{id}', [App\Http\Controllers\PagesController::class, 'delImageFirst'])->name('delImageFirst');
-Route::get('/primeiro-card/download/{id}', [App\Http\Controllers\PagesController::class, 'downImageFirst'])->name('downImageFirst');
+    // Banner routes
+    Route::prefix('/banner')->group(function () {
+        Route::get('/', [App\Http\Controllers\PagesController::class, 'indexBanner'])->name('banner');
+        Route::post('/salvar-nav', [App\Http\Controllers\PagesController::class, 'saveNavbar'])->name('saveNavbar');
+        Route::post('/salvar-banner', [App\Http\Controllers\PagesController::class, 'saveBanner'])->name('saveBanner');
+        Route::post('/salvar-centerbar', [App\Http\Controllers\PagesController::class, 'saveCenterbar'])->name('saveCenterbar');
+    });
+
+    // Firstcard routes
+    Route::prefix('/primeiro-card')->group(function () {
+        Route::get('/', [App\Http\Controllers\PagesController::class, 'indexFirstcard'])->name('indexFirstcard');
+        Route::post('/salvar', [App\Http\Controllers\PagesController::class, 'saveFirstcard'])->name('saveFirstcard');
+        Route::post('/salvar-imagens', [App\Http\Controllers\PagesController::class, 'saveFirstcardImages'])->name('saveFirstcardImages');
+        Route::get('/excluir/{id}', [App\Http\Controllers\PagesController::class, 'delImageFirst'])->name('delImageFirst');
+        Route::get('/download/{id}', [App\Http\Controllers\PagesController::class, 'downImageFirst'])->name('downImageFirst');
+    });
+
+});
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
